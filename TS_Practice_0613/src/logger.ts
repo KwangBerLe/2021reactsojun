3e878ff019b1f9b2b55e409415826f156b296077
KwangBerLe <gamj4525@gmail.com>
2021-06-14 18:44:44 +0900

    20210614TS

---
+++ TS_Practice_0613/src/logger.ts
@@ -0,0 +1,13 @@
+// 복수 객체 내보내기
+
+export function debug(msg: string):void {
+    console.debug(msg);
+  }
+  
+  export function log(msg: string):void {
+    console.log(msg);
+  }
+  
+  export function error(msg: string):void {
+    console.error(msg);
+  }
